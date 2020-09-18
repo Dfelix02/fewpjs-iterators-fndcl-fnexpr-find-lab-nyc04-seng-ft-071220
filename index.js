e22f9bd44cd43d0e2c443861ed976c4bd57c3b8a
David Felix <david.felix02@hotmail.com>
2020-09-18 03:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const testVar = {};
 
 function superbowlWin(arrOfObjs) {
   let gotObj = arrOfObjs.find((obj) => obj.result === "W")
-  if (typeof gotObj.result !== "undefined") {
+  if (typeof gotObj !== "undefined") {
     return gotObj.year
   }
    else { 
